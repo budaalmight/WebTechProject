0804102dd0e3ea51bde76cb7e789052f1e179c1e
Bozhidar Kuzmanov <bozhidar.kuzmanov@novarto.com>
2016-06-27 23:14:00 +0300

    Some design added

--- web/controllers/login.js
+++ web/controllers/login.js
@@ -12,11 +12,24 @@ $(document).ready(function () {
             'success': callback
         });
     };
+    $.getJSON = function (url, data, callback) {
+        return jQuery.ajax({
+            headers:{
+              'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            'type': 'GET',
+            'url': url,
+            'data': JSON.stringify(data),
+            'dataType': 'json',
+            'success': callback
+        });
+    };
     $('#wrong-login').hide();
     $('#login-button').click(function () {
-        var username = $('#login-fn');
+        var username = $('#login-name');
         var u = username.val();
-        var password = $('#login-password');
+        var password = $('#login-pass');
         var p = password.val();
         $.postJSON("/rest/users/login",{username: u, password: p} , function (data, status) {
             localStorage.setItem('sid',data.sid);
